98780e3fc7211f46e66e7949433fdb2422948142
turminha10 <116770516+turminha10@users.noreply.github.com>
2023-03-14 11:32:14 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,36 @@
+
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + ":";
+document.getElementById("player2_name").innerHTML = player2_name + ":";
+
+document.getElementById("player1_score").innerHTML = player1_score ; 
+document.getElementById("player2_score").innerHTML = player2_score ; 
+
+document.getElementById("player_question").innerHTML = "turno de pergunta -" + player1_name;
+document.getElementById("player_answer").innerHTML = "turno de respoosta -" + player2_name;
+
+function send() {
+number1 = document.getElementById("number1").value;
+number2 = document.getElementById("number2").value;
+actual_answer = parseInt(number1)*parseInt (number2);
+console.log(actual_answer);
+
+
+question_number = "<h4>" + number1 + "X"+ number2+ "</h4>";
+input_box = "";
+check_button = "";
+row = question_number + input_box-box  + check_button ;
+document.getElementById("output").innerHTML = row;
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
+}
+
+
+question_turn = "player1";
+answer_turn = "player2";
+
